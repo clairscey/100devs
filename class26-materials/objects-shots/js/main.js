6561a220e9141bbfe922e87926c8ee95593bcdc2
Chris <clairscey@gmail.com>
2022-04-11 21:10:44 -0500

    update hw

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -5,6 +5,11 @@
 // console.log(userDrink)
 
 document.querySelector('button').addEventListener('click', makeDrink)
+document.querySelector("#drinkSearch").addEventListener('keyup', e => {
+  if(e.keyCode === 13){
+    makeDrink();
+  }
+});
 
 function makeDrink() {
     let userDrink = document.querySelector('input').value;
@@ -13,28 +18,18 @@ function makeDrink() {
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
-      document.querySelector('h2').innerText = data.drinks[0].strDrink;
-      document.querySelector('img').src = data.drinks[0].strDrinkThumb;
-      document.querySelector('h3').innerText = data.drinks[0].strInstructions;
+
+      document.querySelector('#0').innerHTML = data.drinks[0].strDrink;
+      document.querySelector('#0 > img').src = data.drinks[0].strDrinkThumb;
+      document.querySelector('#0 > h3').innerText = data.drinks[0].strInstructions;
     
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
-
-
 }
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com',
-		'X-RapidAPI-Key': '47ed5ebf5bmsh9ae2622df944a6fp1a86e4jsnc01b2c407bbb'
-	}
-};
-
-fetch('https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
+function drawMenu(arr) {
+  const drinksMenu = document.querySelector('#drinksMenu');
+  arr.forEach( (e, i) => drinksMenu.
+}
